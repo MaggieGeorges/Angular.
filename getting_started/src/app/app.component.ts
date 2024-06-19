aa57dc066c5f99fdd21aa2432ea4f787a20c4f64
Maggie Georges <maggywangari01@gmail.com>
2024-06-19 13:38:41 +0300

    Updated Home Component

--- getting_started/src/app/app.component.ts
+++ getting_started/src/app/app.component.ts
@@ -1,33 +1,44 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  disable = true;
-  greetins: String = "Hello World Again";
-  text = "Starting text";
-
-  constructor() {
-    setTimeout(() => {
-      this.disable = false;
-    }, 4000);
-  }
-
-  greet() {
-    return "Hi There";
-  }
-
-  AddName() {
-    console.log("Clicked!!");
-  }
-
-  takeInput(event: Event) {
-    console.log(event);
-  }
+  title = 'getting_started'
 }
+
+
+
+
+// export class AppComponent {
+//   disable = true;
+//   greetings: String = "Hello World Again";
+//   text = "Starting text";
+
+//   constructor() {
+//     setTimeout(() => {
+//       this.disable = false;
+//     }, 4000);
+//   }
+
+//   greet() {
+//     return "Hi There";
+//   }
+
+//   AddName() {
+//     console.log("Clicked!!");
+//   }
+
+//   takeInput(event: Event) {
+//     console.log(event);
+//   }
+
+//   textng:string = "eseat"
+// }
--- getting_started/src/app/home/home.component.ts
+++ getting_started/src/app/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  disable = true
+  greetings:String = "Hello World Again!!"
+  text ="Starting text"
 
+  greet(){
+    return  "Hi There!"
+  }
+
+  constructor(){
+    setTimeout(()=>{
+      this.disable=false
+    }, 4000)
+  }
+
+  AddName(){
+    console.log("Clicked!!1")
+  }
+
+  takeInput(event:Event){
+    console.log(event);
+    
+  }
 }
