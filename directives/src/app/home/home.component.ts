8acb5257160ac5267059dc41f3601dbb63436b05
Maggie Georges <maggywangari01@gmail.com>
2024-06-19 16:46:06 +0300

    Adding directives folder

---
+++ directives/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { log } from 'console';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  people: { Name: string, Age: number }[] = []
+  name = ''
+  age = ''
+  day="Sunday"
+  Showredp=false
+
+  add() {
+    this.people.push({ Name: this.name, Age: +this.age })
+    console.log(this.people)
+    this.Showredp= !this.Showredp
+  }
+  deletedPerson(i: number ) {
+    this.people.splice(i, 1)
+  }
+}
